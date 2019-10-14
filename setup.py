c45ca43b023bae47d76568031631998283ebe3d8
ML <michal.j.lipka@gmail.com>
2019-10-14 19:58:06 +0200

    Dodanie w setup.py python3 dla Cythona (defaultowo python2)

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ extensions = [
         ]
 
 setup(
-    ext_modules=cythonize(extensions),
+    ext_modules=cythonize(extensions,compiler_directives={'language_level' : "3"}),
     include_dirs=[numpy.get_include()]
 )
