ee92407ebb9c389e01672eef60e8314e97a117b7
Zyla LPK <nomail@nomail>
2020-02-05 02:24:04 +0100

    lvjson v20.01

--- photonpacket/labviewjsonparse.py
+++ photonpacket/labviewjsonparse.py
@@ -25,20 +25,34 @@ def parse(jsonfile):
     try:
         with open(jsonfile, "r") as f:
             s=f.read()
-            jsondata1, end1 = json.JSONDecoder().raw_decode(s)
-            if type(jsondata1)==dict and jsondata1.get('version','xxx')[:2]=='3.':
-                #print(file)
-                try:
-                    jsondata2, end2 = json.JSONDecoder().raw_decode(s[end1:])
-                    jsondata1.update(jsondata2)
-                except:
-                    pass   
-                if jsondata1['version']=='3.05':
-                    from .lvjson3 import LV305
-                    print('labviewjsonparse LV305')
-                    return LV305(jsondata1)
-                print('labviewjsonparse V3 json')
-                return jsondata1             
+            jsondata1, end1 = json.JSONDecoder().raw_decode(s)            
+            if type(jsondata1)==dict:
+                if str(jsondata1.get('version','xxx'))[:2]=='3.':
+                    print(jsonfile)
+                    try:
+                        jsondata2, end2 = json.JSONDecoder().raw_decode(s[end1:])
+                        jsondata1.update(jsondata2)
+                    except:
+                        pass   
+                    if jsondata1['version']=='3.05':
+                        from .lvjson3 import LV305
+                        print('labviewjsonparse LV305')
+                        return LV305(jsondata1)
+                    print('labviewjsonparse V3 json')
+                    return jsondata1    
+                elif str(jsondata1.get('version','xxx'))[:3]=='20.':
+                    try:
+                        jsondata2, end2 = json.JSONDecoder().raw_decode(s[end1:])
+                        jsondata1.update(jsondata2)
+                    except:
+                        pass   
+                    if str(jsondata1['version'])=='20.01':
+                        from .lvjson20 import LV2001
+                        print('labviewjsonparse LV2001')
+                        return LV2001(jsondata1)
+                    print('labviewjsonparse V20 json')
+                    return jsondata1  
+                    
     except Exception as error:
             print('labviewjsonparse.parse:',error)
             #TODO raise exception?
